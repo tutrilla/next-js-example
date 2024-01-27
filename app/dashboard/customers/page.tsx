bd4424f7a2ab5f8d4f3dff5f7cf7d1c94ddb97f2
tutrilla <145156699+tutrilla@users.noreply.github.com>
2024-01-26 18:44:48 -0700

    fixed customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
-export default function() {
+// customers/page.tsx
+
+const CustomersPage: React.FC = () => {
     return <p>Customers Page</p>;
-}
+};
+
+CustomersPage.displayName = 'CustomersPage';
+
+export default CustomersPage;
